e0f63ebac20059a7716fabb5b97cbb605c57aba3
ducviet <vietnd@phenikaa-x.com>
2024-10-15 23:46:14 +0700

    ..

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,36 +1,27 @@
 const { connection } = require("../config/database");
+const { getAllUser, getUserById } = require("../services/CRUDService");
 
 const getHomepage = (req, res) => {
   return res.render("home.ejs");
 };
 
 const getlistPage = async (req, res) => {
-  const [results, fields] = await connection.query(`select * from Users`);
-  console.log("results", results);
-  return res.render("listUsers.ejs", { listUsers: results });
+  const dataUser = await getAllUser();
+  console.log('dataUser',dataUser);
+  return res.render("listUsers.ejs", { listUsers: dataUser });
 };
+
+const getUpdatePage = async (req, res) => {
+    const userId = req.params.id;
+    let user = await getUserById(userId);
+    res.render('EditUser.ejs', { userEdit: user }); //x <- y
+}
+
 
 const postCreateUser = async (req, res) => {
   let email = req.body.email;
   let name = req.body.name;
   let city = req.body.City;
-
-  //   console.log("email", email);
-  //   console.log("name", name);
-  //   console.log("city", city);
-  //   connection.query(
-  //     `INSERT INTO
-  //       Users (email, name, city)
-  //        VALUES (?,?,?)`,
-  //     [email, name, city],
-  //     function (err, results) {
-  //       res.send("oce r đó");
-  //     }
-  //   );
-
-  //   const [rows, fields] = await connection.execute("SELECT * FROM Users");
-  //   console.log('row',rows);
-  //   console.log('fields',fields);
 
   let [results, fields] = await connection.query(
     `INSERT INTO
@@ -47,4 +38,5 @@ module.exports = {
   getHomepage,
   postCreateUser,
   getlistPage,
+  getUpdatePage
 };
