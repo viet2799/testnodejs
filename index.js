e8dfec1951082c4cd92a7f18c130bba18b7e5989
Nguyễn Đức Việt <43706629+viet2799@users.noreply.github.com>
2024-10-16 19:32:47 +0700

    .

--- index.js
+++ index.js
@@ -3,36 +3,31 @@ const express = require("express");
 const configViewEngine = require("./src/config/viewEngine");
 const webRoutes = require("./src/routes/api");
 const { connection } = require("./src/config/database");
+const { default: mongoose } = require("mongoose");
 const app = express();
+
 const port = 3000;
 configViewEngine(app);
 app.use("/", webRoutes);
-const mongoose = require('mongoose');
-// connection.query("SELECT * FROM Users", (err, results) => {
-//   if (err) {
-//     console.error("Error executing q uery:", err);
-//     return;
-//   }
-// });
-const kittySchema = new mongoose.Schema({
-    name: String
-});
-const Kitten = mongoose.model('Kitten', kittySchema);
-const cat = new Kitten({ name: 'Hoi Dan IT cat ' });
-cat.save();
 
+const kittySchema = new mongoose.Schema({
+  name: String,
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const cat = new Kitten({ name: 'Silence' });
+console.log(cat.name); // 'Silence'
+
+ cat.save();
 
 (async () => {
   try {
     await connection();
-    app.listen(port, hostname, () => {
-      console.log(`Backend zero app listening on port ${port}`);
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
     });
   } catch (error) {
-    console.log(">>> Error connect to DB: ", error);
+    console.log(error);
   }
 })();
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
--- src/config/database.js
+++ src/config/database.js
@@ -1,42 +1,43 @@
-require("dotenv").config();
-const { mongo, default: mongoose } = require("mongoose");
 
-const dbState = [
-  {
+require('dotenv').config();
+const { mongo, default: mongoose } = require('mongoose');
+
+const dbState = [{
     value: 0,
-    label: "Disconnected",
-  },
-  {
+    label: "disconnected"
+},
+{
     value: 1,
-    label: "Connected",
-  },
-  {
+    label: "connected"
+},
+{
     value: 2,
-    label: "Connecting",
-  },
-  {
+    label: "connecting"
+},
+{
     value: 3,
-    label: "Disconnecting",
-  },
-];
+    label: "disconnecting"
+}];
+
 
 const connection = async () => {
-  try {
-    const options = {
-      pass: process.env.DB_PASSWORD,
-      dbName: process.env.DB_NAME,
-    };
-    await mongoose.connect(process.env.DB_HOST, options);
-    const state = Number(mongoose.connection.readyState);
-    console.log(dbState.find((f) => f.value == state).label, "to db"); // connected to db
+    try {
+        const  options={
+            user: process.env.DB_USER,
+            pass: process.env.DB_PASSWORD,
+            dbName: process.env.DB_NAME,
+        }
+        await mongoose.connect(process.env.DB_HOST, options);
+        const state = Number(mongoose.connection.readyState);
+        console.log(dbState.find(f => f.value == state).label, "to db"); // connected to db
+        console.log('Connect to database successfully');
+    }catch (error) {
+        console.log('Connect to database failed');
+        console.log(error);
+    }
+}
 
-    console.log("Connect to database successfully");
-  } catch (error) {
-    console.log("Connect to database failed");
-    console.log(error);
-  }
-};
 
 module.exports = {
-  connection,
-};
+    connection
+}
